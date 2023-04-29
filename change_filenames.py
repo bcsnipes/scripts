450566b35ea8996bec55512de851481b9370a91d
bcsnipes <brettsnipes@yahoo.com>
2023-04-29 14:43:44 -0400

    Optimize

--- change_filenames.py
+++ change_filenames.py
@@ -2,17 +2,19 @@ import datetime
 import os
 import sys
 
-def change_filenames(path, prefix, extension = ".jpg"):
-  for file_name in os.listdir(path):
-    birthtime = os.stat(f"{path}/{file_name}").st_birthtime
-    converted_datetime = datetime.datetime.fromtimestamp(birthtime)
-
-    formatted = (converted_datetime.strftime(f'{prefix}_%y%m%d_%H%M%S'))
-    source = f"{path}/{file_name}"
-    destination = f"{path}/{formatted}.{extension}"
-
-    os.rename(source, destination)
+def change_filenames(path):
+    for entry in os.scandir(path):
+        if entry.is_file():
+            source = entry.path
+            birthtime = entry.stat().st_birthtime
+            converted_datetime = datetime.datetime.fromtimestamp(birthtime)
+            formatted = converted_datetime.strftime(f"%y%m%d_%H%M%S")
+            extension = os.path.splitext(source)[1]
+            destination = os.path.join(path, f"{formatted}{os.path.splitext(source)[1]}")
+            if source != destination:
+                os.rename(source, destination)
 
 if __name__ == "__main__":
-  change_filenames(sys.argv[1], sys.argv[2], sys.argv[3])
+    change_filenames(sys.argv[1])
+
 
